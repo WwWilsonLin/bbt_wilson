ad3a4890c4cc5b9edef5954b716a798858d1d601
WwWilsonLin <847799751@qq.com>
2017-01-24 17:44:58 +0800

    代码优化，尝试移动端适配

--- chessboard.js
+++ chessboard.js
@@ -1,5 +1,6 @@
 var turn = "b";
 var n = 0;  //记录落子个数
+var winner = "";
 var storage = [];
 
 var arr = new Array();          
@@ -34,58 +35,50 @@ print();
 //落子
 canvas.onclick = function(e){
  	e = e || window.event;
+ 	//获取坐标
  	var rect = canvas.getBoundingClientRect();   
  	var x = e.clientX - rect.left * (canvas.width / rect.width);
     var y = e.clientY - rect.top * (canvas.height / rect.height);
-	//console.log(x);console.log(y);
 	var chess_x = Math.round(x/25) * 25;
 	var chess_y = Math.round(y/25) * 25;
-	//console.log(chess_x);console.log(chess_y);
-	if (chess_x == 0 || chess_x == 500 || chess_y == 0 || chess_y == 500 || arr[chess_x][chess_y] !== ""){}
+
+	if (chess_x == 0 || chess_x == 500 || chess_y == 0 || chess_y == 500 || arr[chess_x][chess_y] !== "" || winner !== ""){}
 	else{
 		if (turn == "w"){
 			document.getElementById('message').innerHTML = '轮到黑方了';
-			white(chess_x,chess_y);
+			chess("white",chess_x,chess_y);
 			check("white",chess_x,chess_y);
 			n++;
 			turn = "b";
 		}
 		else{
 			document.getElementById('message').innerHTML = '轮到白方了';
-			black(chess_x,chess_y);
+			chess("black",chess_x,chess_y);
 			check("black",chess_x,chess_y);
 			n++;
 			turn = "w";
 		}
-		console.log(storage);
 	} 
- }
- //白棋落子
- function white(chess_x,chess_y){	
+}
+//打印棋子
+function chess(s,chess_x,chess_y){	
 	cxt.beginPath();
 	cxt.arc(chess_x,chess_y,12,0,360,false);
 	cxt.lineWidth = 1;
-	cxt.fillStyle = "#FFF";
+	if (s == "white"){
+		cxt.fillStyle = "#FFF";
+	}
+	else if (s == "black"){
+		cxt.fillStyle = "#000";
+	}
 	cxt.fill();
 	cxt.strokeStyle = "#000";
 	cxt.stroke();
 	cxt.closePath();
-	arr[chess_x][chess_y] = "white";
-	store(n,"white",chess_x,chess_y);
+	arr[chess_x][chess_y] = s;
+	store(n,s,chess_x,chess_y);
 }
-//黑棋落子
-function black(chess_x,chess_y){
-	cxt.beginPath();
-	cxt.arc(chess_x,chess_y,12,0,360,false);
-	cxt.lineWidth = 1;
-	cxt.fillStyle = "#000";
-	cxt.fill();
-	cxt.strokeStyle = "#000";
-	cxt.stroke();
-	cxt.closePath();
-	arr[chess_x][chess_y] = "black";
-	store(n,"black",chess_x,chess_y);
-}
+
 //胜负判定
 function check(s,x,y){
 	var k=0;
@@ -96,7 +89,7 @@ function check(s,x,y){
 		if (arr[i*25][y] == s){
 			k++;
 			if (k == 5){
-				document.getElementById('message').innerHTML = s + ' WIN!!!';
+				win(s);
 				break;
 			}
 		}
@@ -105,11 +98,12 @@ function check(s,x,y){
 		}
 	}
 	//竖
+	k = 0;
 	for(var j = (arry - 4); j < (arry + 5); j++){
 		if (arr[x][j*25] == s){
 			k++;
 			if (k == 5){
-				document.getElementById('message').innerHTML = s + ' WIN!!!';
+				win(s);
 				break;
 			}
 		}
@@ -118,11 +112,12 @@ function check(s,x,y){
 		}
 	}
 	//斜45
+	k = 0;
 	for(var i = -4; i < 5; i++){
 		if (arr[x - (i * 25)][y + (i * 25)] == s){
 			k++;
 			if (k == 5){
-				document.getElementById('message').innerHTML = s + ' WIN!!!';
+				win(s);
 				break;
 			}
 		}
@@ -131,17 +126,27 @@ function check(s,x,y){
 		}
 	}
 	//斜135
+	k = 0;
 	for(var i = -4; i < 5; i++){
 		if (arr[x + (i * 25)][y + (i * 25)] == s){
 			k++;
 			if (k == 5){
-				document.getElementById('message').innerHTML = s + ' WIN!!!';
+				win(s);
 				break;
 			}
 		}
 		else {
 			k = 0;
 		}
+	}
+}
+function win(s){
+	winner = s;
+	if (s == "white"){
+		document.getElementById('message').innerHTML = "白方赢了!!!";
+	}
+	else if (s == "black"){
+		document.getElementById('message').innerHTML = "黑方赢了!!!";
 	}
 }
 //存储
@@ -190,4 +195,8 @@ read.onclick = function(){
 		}
 	}
 	console.log(arr);
+}
+var newg = document.getElementById("new");
+newg.onclick = function(){
+	window.location = "五子棋.html"
 }
